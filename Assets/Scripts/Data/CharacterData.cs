d37afb5d22bee019c1d45dd713f405aaec5f0806
Drako Vishious <drkmewyt@gmail.com>
2025-05-15 17:10:51 -0500

    Update 0.0.1.A:
    Created Scripts folder, data folder, and main scene. Created CharacterData script, created variables for name, health, mana, and action points. Created CharacterControl script and charData variable. Created level variable.

---
+++ Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData 
+{
+    public string characterName;
+
+    public int level;
+
+    public int maxHealth;
+    public int currentHealth;
+    
+    public int maxMana;
+    public int currentMana;
+
+    public float maxAP;
+    public float currentAP;
+
+
+
+}
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 ## Patch Notes
 Update 0.0.1: Created Repository, Unity Project, and linked them together. Pushed Repository to Github.
+
+Update 0.0.1.A: Created Scripts folder, data folder, and main scene. Created CharacterData script, created variables for name, health, mana, 
+and action points. Created CharacterControl script and charData variable. Created level variable.
